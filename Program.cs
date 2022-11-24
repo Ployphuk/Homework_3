16c10a34a02009ac0199385f8ac0260260e5e6e7
unknown <ployphuk.jafarx@mail.kmutt.ac.th>
2022-11-24 22:08:29 +0700

    fixed

--- Program.cs
+++ Program.cs
@@ -7,38 +7,35 @@ class Program{
         Queue<int>size_3 = new Queue<int>();
 
         char size = 'x';
-        int L=1, M=2, S=3;
+
         
         while( size == 'S' || size == 'M' || size == 'L' || size == 'a' ){
             size = char.Parse(Console.ReadLine());
-            
+
             if(size == 'S'){
-                size_1.Push(S);
+                size_1.Push(3);
             }
             else if (size == 'M'){
-                size_1.Push(M);
+                size_1.Push(2);
                 for (int i = 0; i < 3; i++){
-                    size_2.Push(S);
+                    size_2.Push(3);
                 }
             }
             else if (size == 'L'){
-                size_1.Push(L);
+                size_1.Push(1);
                 for(int i = 0; i<2; i++){
-                    size_2.Push(M);
+                    size_2.Push(2);
                     for(int j =0; j<3; j++){
-                        size_3.Push(S);
+                        size_3.Push(3);
                     }
                 }
             }
-        }
-        for(int n=0; n < size_1.GetLength(); n++){
-            Console.Write("{0}",size_1.Get(n));
         }
         
         PrintInfo(size_1,size_2,size_3);
     }
 
-    static void PrintInfo(Queue<int> size_1,Queue<int> size_2,Queue<int> size_3){
+    public static void PrintInfo(Queue<int> size_1,Queue<int> size_2,Queue<int> size_3){
         for(int s1 =0; s1< size_1.GetLength(); s1++){
             Console.Write("{0}",size_1.Get(s1));
         }
